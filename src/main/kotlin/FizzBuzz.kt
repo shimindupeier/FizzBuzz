2534b93c1c15b1b7f912ce2fc5f309e46f928f7d
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-10-18 19:32:11 +0100

    refactored

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,17 +1,14 @@
 fun fizzBuzz(startInt: Int, endInt: Int): String {
 
-    if (startInt < 0 || endInt < 0)
-        return "Please only enter positive integer"
-    else if (startInt > endInt)
-        return "Start int must be smaller than end int"
-    val numList = (startInt..endInt)
-    val output = mutableListOf<String>()
-    numList.forEach { when {
-        it.rem(3) == 0 && it.rem(5) != 0 -> output.add("fizz")
-        it.rem(3) != 0 && it.rem(5) == 0 -> output.add("buzz")
-        it.rem(3) == 0 && it.rem(5) == 0 -> output.add("fizzbuzz")
-        else -> output.add(it.toString())
-    } }
+    if (startInt < 0 || endInt < 0) return "Please only enter positive integer"
+    else if (startInt > endInt) return "Start int must be smaller than end int"
 
-    return output.joinToString(", ")
+    return ((startInt..endInt).map {
+        when {
+            it.rem(3) == 0 && it.rem(5) == 0 -> "fizzbuzz"
+            it.rem(3) == 0 -> "fizz"
+            it.rem(5) == 0 -> "buzz"
+            else -> it.toString()
+        }
+    }).joinToString(", ")
 }
