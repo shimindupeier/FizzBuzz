e46ad40571c3d41209806494098cd1abded42ea4
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-10-18 21:17:59 +0100

    include test input 0

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -5,6 +5,7 @@ fun fizzBuzz(startInt: Int, endInt: Int): String {
 
     return ((startInt..endInt).map {
         when {
+            startInt == 0 -> "0"
             hasRem(3, it) && hasRem(5, it) -> "fizzbuzz"
             hasRem(3, it) -> "fizz"
             hasRem(5, it) -> "buzz"
--- src/test/kotlin/FizzBuzzKtTest.kt
+++ src/test/kotlin/FizzBuzzKtTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class FizzBuzzKtTest {
 
@@ -6,28 +7,28 @@ internal class FizzBuzzKtTest {
     fun fizzBuzzTestRangeOneToThreeExpectedString() {
         val expected = "1, 2, fizz"
         val result = fizzBuzz(1, 3)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzBuzzTestRangeOneToFifteenExpectedString() {
         val expected = "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
         val result = fizzBuzz(1, 15)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzBuzzTestRangeThreeToFifteenExpectedString() {
         val expected = "fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
         val result = fizzBuzz(3, 15)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzbuzzTestNegativeStartIntReturnErrorMessage() {
         val expected = "Please only enter positive integer"
         val result = fizzBuzz(-1, 3)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
 
     }
 
@@ -35,27 +36,34 @@ internal class FizzBuzzKtTest {
     fun fizzbuzzTestNegativeEndIntReturnErrorMessage() {
         val expected = "Please only enter positive integer"
         val result = fizzBuzz(1, -3)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzbuzzTestNegativeStartIntAndEndIntReturnErrorMessage() {
         val expected = "Please only enter positive integer"
         val result = fizzBuzz(-5, -15)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzBuzzStartIntSmallerThanEndItReturnErrorMessage() {
         val expected = "Start int must be smaller than end int"
         val result = fizzBuzz(10, 1)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
     }
 
     @Test
     fun fizzBuzzStartIntEqualEndInt() {
         val expected = "fizz"
         val result = fizzBuzz(3, 3)
-        kotlin.test.assertEquals(expected, result)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun fizzBuzzTestZeroValue() {
+        val expected = "0"
+        val result = fizzBuzz(0, 0)
+        assertEquals(expected, result)
     }
 }
